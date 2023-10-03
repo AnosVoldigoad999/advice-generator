fbaf5b56652719da12048217997800273041d74a
AnosVoldigoad999 <oyedejiuzaymah@gmail.com>
2023-10-03 18:28:21 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 export default function App (){
-  const [advice, setAdvice] = useState()
+  const [advice, setAdvice] = useState({advice:"", id:""})
   const getData = async ()=>{
     await axios.get('https://api.adviceslip.com/advice').then(response=>{setAdvice(response.data.slip)})
   }
